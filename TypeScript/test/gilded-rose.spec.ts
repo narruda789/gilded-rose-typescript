fc628f0525dd829fb0eee2bb5d146931d3eeeebd
Nicole Arruda <narruda789@gmail.com>
2019-08-06 12:48:40 -0400

    Refactoring tests

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,25 +1,27 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Gilded Rose', function () {
-    var gildedRose: GildedRose;
-    
-    beforeEach(() => {
-        gildedRose = new GildedRose([new Item('banana', 5, 10)]);
+describe('Gilded Rose', () => {
+    describe('happy path', () => {
+        var gildedRose: GildedRose;
+        
+        beforeEach(() => {
+            gildedRose = new GildedRose([new Item('banana', 5, 10)]);
+        });
+        
+        it('should add a banana', () => {
+            const actualUpdatedItem = gildedRose.updateQuality()[0];
+            expect(actualUpdatedItem.name).to.equal('banana');
+        });
+        
+        it('should degrade SellIn from 5 to 4 after one day', () => {
+            const actualUpdatedItem = gildedRose.updateQuality()[0];
+            expect(actualUpdatedItem.sellIn).to.equal(4);
+        });
+        
+        it('should degrade Quality from 10 to 9 after one day',() => {
+            const actualUpdatedItem = gildedRose.updateQuality()[0];
+            expect(actualUpdatedItem.quality).to.equal(9);
+        });
     });
-    
-    it('should add a banana', () => {
-        const actualUpdatedItem = gildedRose.updateQuality()[0];
-        expect(actualUpdatedItem.name).to.equal('banana');
-    });
-
-    it('should degrade SellIn from 5 to 4 after one day', () => {
-        const actualUpdatedItem = gildedRose.updateQuality()[0];
-        expect(actualUpdatedItem.sellIn).to.equal(4);
-    });
-
-    it('should degrade Quality from 10 to 9 after one day',() => {
-        const actualUpdatedItem = gildedRose.updateQuality()[0];
-        expect(actualUpdatedItem.quality).to.equal(9);
-    })
 });
