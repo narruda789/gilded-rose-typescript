573f78aae5308867ba309eff130cbec81d75abbe
Nicole Arruda <narruda789@gmail.com>
2019-08-06 12:46:02 -0400

    Test to degrade Quality after one day

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -2,15 +2,24 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-    const gildedRose = new GildedRose([new Item('banana', 5, 10)]);
+    var gildedRose: GildedRose;
+    
+    beforeEach(() => {
+        gildedRose = new GildedRose([new Item('banana', 5, 10)]);
+    });
     
     it('should add a banana', () => {
         const actualUpdatedItem = gildedRose.updateQuality()[0];
         expect(actualUpdatedItem.name).to.equal('banana');
     });
 
-    it('should degrade SellIn from 5 to 4 after one day', () =>{
+    it('should degrade SellIn from 5 to 4 after one day', () => {
         const actualUpdatedItem = gildedRose.updateQuality()[0];
         expect(actualUpdatedItem.sellIn).to.equal(4);
     });
+
+    it('should degrade Quality from 10 to 9 after one day',() => {
+        const actualUpdatedItem = gildedRose.updateQuality()[0];
+        expect(actualUpdatedItem.quality).to.equal(9);
+    })
 });
